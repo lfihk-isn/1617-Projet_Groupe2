162f67c927437c37098d024a131adbfe52547fdc
theo2947 <theo2947@lfis.edu.hk>
2017-04-21 15:23:07 +0800

    changements sur HTML
    changements sur HTML

--- html/lumiere.php
+++ html/lumiere.php
@@ -1,16 +1,16 @@
 <!DOCTYPE html>
 <html>
 	<head>
-	<link rel="stylesheet" type="text/css" href="VO.css">
+	<link rel="stylesheet" type="text/css" href="lumiere.css">
 		<title>widget maison connectée</title>
 	</head>
 	<body>
 		<h1>Maison connectée v.1<br>Projet ISN</h1>
 		<p>Ici vous pouvez controler directement vos différents volets:</p>
-		<img src="volets logo.png" alt="lamp" style="width:204px;height:228px;">
+		<img src="lamp.png" alt="lamp" style="width:304px;height:288px;">
 	<h2>
 		<meta name="viewport" content="width=device-width" />
-				<form method="get" action="index.php">
+				<form method="get" action="lumiere.php">
 						<input type="submit" value="I" name="on" class="btn0 button">
 						<input type="submit" value="O" name="off" class="btn0 button">
 				</form>
@@ -28,7 +28,7 @@
 	</h2>
 	<h2>
 		<meta name="viewport" content="width=device-width" />
-				<form method="get" action="index.php">
+				<form method="get" action="lumiere.php">
 						<input type="submit" value="I" name="on1" class="btn0 button">
 						<input type="submit" value="O" name="off1" class="btn0 button">
 				</form>
