6e686cac447d7f7fa5e46df713cbd1d12c6540dc
theo2947 <theo2947@lfis.edu.hk>
2017-04-28 13:44:39 +0800

    HTML 2 (final?)
    html modifie par jo

---
+++ html2/lumieres.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	<link rel="stylesheet" type="text/css" href="VO.css">
+		<title>M.C.: Lumieres</title>
+	<link rel="icon" type="image/png" href="lamp.png" />
+	</head>
+	<body>
+		<h1>Maison connectée v.1<br>Projet ISN</h1>
+		<p>Ici vous pouvez controler directement vos différentes lumieres:</p>
+		<img src="lamp.png" alt="lamp" style="width:204px;height:228px;">
+		<p>Lumiere 1:</p>
+	<h2>
+		<meta name="viewport" content="width=device-width" />
+				<form method="get" action="lumieres.php">
+						<input type="submit" value="I" name="on" class="btn0 button">
+						<input type="submit" value="O" name="off" class="btn0 button">
+				</form>
+				<?php
+				$setmode2 = shell_exec("/usr/local/bin/gpio -g mode 2 out");
+				if(isset($_GET['on'])){
+						$gpio_on = shell_exec("/usr/local/bin/gpio -g write 2 1");
+						echo "Lumiere allumee";
+				}
+				else if(isset($_GET['off'])){
+						$gpio_off = shell_exec("/usr/local/bin/gpio -g write 2 0");
+						echo "Lumiere eteinte";
+				}
+				?>
+	</h2>
+	<p>Lumiere 2:</p>
+	<h2>
+		<meta name="viewport" content="width=device-width" />
+				<form method="get" action="lumieres.php">
+						<input type="submit" value="I" name="on1" class="btn0 button">
+						<input type="submit" value="O" name="off1" class="btn0 button">
+				</form>
+				<?php
+				$setmode2 = shell_exec("/usr/local/bin/gpio -g mode 3 out");
+				if(isset($_GET['on1'])){
+						$gpio_on = shell_exec("/usr/local/bin/gpio -g write 3 1");
+						echo "Lumiere allumee";
+				}
+				else if(isset($_GET['off1'])){
+						$gpio_off = shell_exec("/usr/local/bin/gpio -g write 3 0");
+						echo "Lumiere eteinte";
+				}
+				?>
+				 
+				 <p> </p>
+				 
+		
+	</h2>
+	<h2><a href="index.html"><button type="button" class="btn3 button"><span>Retour a la page d'accueil</button> </a> </p></h2>
+	
+	<body>
+	</head>
+</html>
+
---
+++ html2/ventilateurs.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	<link rel="stylesheet" type="text/css" href="VE.css">
+		<title>M.C.: Ventilateurs</title>
+	<link rel="icon" type="image/png" href="ventilo.png" />
+	</head>
+	<body>
+		<h1>Maison connectée v.1<br>Projet ISN</h1>
+		<p>Ici vous pouvez controler directement vos différents ventilateurs:</p>
+		<img src="ventilo.png" alt="lamp" style="width:304px;height:288px;">
+	<h2>
+		<meta name="viewport" content="width=device-width" />
+				<form method="get" action="ventilateurs.php">
+						<input type="submit" value="I" name="on" class="btn0 button">
+						<input type="submit" value="O" name="off" class="btn0 button">
+				</form>
+				<?php
+				$setmode2 = shell_exec("/usr/local/bin/gpio -g mode 2 out");
+				if(isset($_GET['on'])){
+						$gpio_on = shell_exec("/usr/local/bin/gpio -g write 2 1");
+						echo "Lumiere allumee";
+				}
+				else if(isset($_GET['off'])){
+						$gpio_off = shell_exec("/usr/local/bin/gpio -g write 2 0");
+						echo "Lumiere eteinte";
+				}
+				?>
+				
+	</h2>
+	<h2><a href="index.html"><button type="button" class="btn3 button"><span>Retour a la page d'accueil</button> </a> </p></h2>
+	
+	<body>
+	</head>
+</html>
